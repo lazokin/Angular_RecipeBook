4ba612a673013a8a85343f50f10aac0376fb0a86
Nik Ambukovski <lazokin@hotmail.com>
2019-01-13 18:42:52 +1100

    display list of recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -14,6 +14,11 @@ export class RecipeListComponent implements OnInit {
       `Take your taste buds to Mexico with these flavoursome tacos. Succulent chicken breast fillets are coated in spicy Chipotle
        seasoning for a weeknight dinner your family will love!`,
       'https://img.taste.com.au/fF1NhAd6/w643-h428-cfill-q90/taste/2018/12/chipotle-chicken-tacos-144989-1.jpg'
+    ),
+    new Recipe(
+      'Fish tacos',
+      'Fish makes a healthy taco filling, and it tastes absolutely delicious.',
+      'https://img.taste.com.au/qqy4eglS/w643-h428-cfill-q90/taste/2016/11/fish-tacos-95173-1.jpeg'
     )
   ];
 
