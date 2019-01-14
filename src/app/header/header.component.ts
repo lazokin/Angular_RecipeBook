e9790146bba92a30137e0776ea554099d76f4c81
Nik Ambukovski <lazokin@hotmail.com>
2019-01-14 11:39:04 +1100

    add two page navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
     selector: 'app-header',
@@ -7,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+    private feature: String = 'recipes';
+    @Output() featureChanged = new EventEmitter<string>();
+
     constructor() { }
 
     ngOnInit(): void {}
+
+    onClick(feature: string) {
+        this.feature = feature;
+        this.featureChanged.emit(feature);
+    }
 
 }
