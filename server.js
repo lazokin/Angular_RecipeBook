8e9a024db0e150de5fe0d8605da0f17fd698a054
Nik Ambukovski <lazokin@hotmail.com>
2019-01-19 15:15:27 +1100

    prepare for heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+
+var path = __dirname + "/dist/";
+
+app.use(express.static(path));
+
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
+
+app.get('*', function(req, res) {
+    res.sendFile(path + "index.html")
+  })
