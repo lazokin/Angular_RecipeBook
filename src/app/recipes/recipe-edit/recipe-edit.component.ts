e471f5bc00ac1465cc0e1fbf21486a3038bdf503
Nik Ambukovski <lazokin@hotmail.com>
2019-01-21 16:17:30 +1100

    create recipe edit form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { RecipeService } from 'src/app/shared/service/recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -10,17 +12,42 @@ export class RecipeEditComponent implements OnInit {
 
   private id: number;
   private editMode: boolean;
+  private form: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private service: RecipeService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(
       (paramMap: ParamMap) => {
         this.id = +paramMap.get('id');
         this.editMode = paramMap.has('id');
-        console.log(this.editMode);
+        this.initialiseForm();
       }
     );
+  }
+
+  onSubmit() {
+    console.log(this.form);
+  }
+
+  private initialiseForm() {
+    
+    let name = '';
+    let description = '';
+    let imageUrl = '';
+
+    if (this.editMode) {
+      let recipe = this.service.getRecipe(this.id);
+      name = recipe.name;
+      description = recipe.description;
+      imageUrl = recipe.imageUrl;
+    }
+
+    this.form = new FormGroup({
+      'name': new FormControl(name),
+      'description': new FormControl(description),
+      'imageUrl': new FormControl(imageUrl)
+    });
   }
 
 }
--- src/app/shared/service/recipe.service.ts
+++ src/app/shared/service/recipe.service.ts
@@ -9,8 +9,7 @@ export class RecipeService {
     private recipes: Recipe[] = [
         new Recipe(
             'Chipotle chicken tacos',
-            `Take your taste buds to Mexico with these flavoursome tacos. Succulent chicken breast fillets are coated in spicy Chipotle
-            seasoning for a weeknight dinner your family will love!`,
+            `Take your taste buds to Mexico with these flavoursome tacos. Succulent chicken breast fillets are coated in spicy Chipotle seasoning for a weeknight dinner your family will love!`,
             'https://img.taste.com.au/fF1NhAd6/w643-h428-cfill-q90/taste/2018/12/chipotle-chicken-tacos-144989-1.jpg',
             [
                 new Ingredient('Chicken', 1),
